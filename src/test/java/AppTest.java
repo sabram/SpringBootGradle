26498a95a30f0198e625b7c3329d377c1e28ddeb
sabram <shaunabram@gmail.com>
2014-07-27 21:16:40 -0700

    initial commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,20 @@
+package com.shaunabram.springboot;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
